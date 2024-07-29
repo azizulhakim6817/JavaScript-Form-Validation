b2e61cb66c80b62451904c05981e9f4d0b28e659
Azizul Hakim <148012067+azizulhakim6817@users.noreply.github.com>
2024-07-29 06:50:24 +0600

    Validate-Numeric-Input

---
+++ index.js
@@ -0,0 +1,21 @@
+function myFunction() {
+  // Get the value of the input field with id="input"
+  let x = document.getElementById("input").value;
+  // If x is Not a Number or less than one or greater than 10
+  let text;
+  if (isNaN(x) || x < 1 || x > 10) {
+    text = "Input not valid";
+  } else {
+    text = "Input OK";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
+
+/* JavaScript Form Validation */
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
